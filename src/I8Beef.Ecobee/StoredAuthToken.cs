bc6f9416e10e0b3f1e13bed1d1c717633c7f7ed1
Bortxx <3203210+Bortxx@users.noreply.github.com>
2018-04-09 17:00:55 -0700

    Create StoredAuthToken.cs

---
+++ src/I8Beef.Ecobee/StoredAuthToken.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace I8Beef.Ecobee.Messages
+{
+    /// <summary>
+    /// Ecobee authorization token data that needs to be stored.
+    /// </summary>
+    public class StoredAuthToken
+    {
+        /// <summary>
+        /// Ecobee authorization token.
+        /// </summary>
+        public string AuthToken { get; set; }
+
+        /// <summary>
+        /// Ecobee refresh token.
+        /// </summary>
+        public string RefreshToken { get; set; }
+
+        /// <summary>
+        /// Ecobee token expiration time.
+        /// </summary>
+        public DateTime TokenExpiration { get; set; }
+    }
+}
