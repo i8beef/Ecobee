e6bf7011ff1774a04ff0e2ba595956f0f00028cb
Bortxx <3203210+Bortxx@users.noreply.github.com>
2018-04-10 00:43:41 -0700

    Update Program.cs

--- src/I8Beef.Ecobee.TestClient/Program.cs
+++ src/I8Beef.Ecobee.TestClient/Program.cs
@@ -1,5 +1,8 @@
+using I8Beef.Ecobee.Messages;
+using I8Beef.Ecobee.Protocol;
 using I8Beef.Ecobee.Protocol.Thermostat;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -10,7 +13,7 @@ namespace I8Beef.Ecobee.TestClient
         static void Main(string[] args)
         {
             var appKey = "";
-            StoredAuthToken storedAuthToken;          
+            StoredAuthToken storedAuthToken;
 
             if (File.Exists(@"token.txt"))
             {
@@ -45,7 +48,8 @@ namespace I8Beef.Ecobee.TestClient
             // Setup client
             var client = new Client(appKey, storedAuthToken, (o) => { WriteTokenFile(o); });
 
-            Console.WriteLine("Getting thermostat information");
+            // Get thermostat summary
+            Console.WriteLine("Getting thermostat summary");
 
             var request = new ThermostatSummaryRequest
             {
@@ -56,9 +60,66 @@ namespace I8Beef.Ecobee.TestClient
             };
 
             var response = client.GetAsync<ThermostatSummaryRequest, ThermostatSummaryResponse>(request).Result;
-
             Console.WriteLine();
             Console.WriteLine(JsonSerializer<ThermostatSummaryResponse>.Serialize(response));
+
+            // Set the HvacMode
+            //Console.WriteLine("Setting thermostat HVAC mode");
+
+            //var updateRequest = new ThermostatUpdateRequest
+            //{
+            //    Selection = new Protocol.Objects.Selection
+            //    {
+            //        SelectionType = "registered"
+            //    },
+            //    Thermostat = new { Settings = new { HvacMode = "auto" } }
+            //};
+
+            //var updateResponse = client.PostAsync<ThermostatUpdateRequest, Response>(updateRequest).Result;
+            //Console.WriteLine();
+            //Console.WriteLine(JsonSerializer<Response>.Serialize(updateResponse));
+
+            // Get thermostat settings
+            //Console.WriteLine("Getting thermostat information");
+
+            //var theroRequest = new ThermostatRequest
+            //{
+            //    Selection = new Protocol.Objects.Selection
+            //    {
+            //        SelectionType = "registered",
+            //        IncludeSettings = true
+            //    }
+            //};
+
+            //var thermoResponse = client.GetAsync<ThermostatRequest, ThermostatResponse>(theroRequest).Result;
+            //Console.WriteLine();
+            //Console.WriteLine(JsonSerializer<ThermostatResponse>.Serialize(thermoResponse));
+
+            // Set thermostat fan
+            //Console.WriteLine("Setting thermostat fan hold");
+
+            //var themroFanRequest = new ThermostatUpdateRequest
+            //{
+            //    Selection = new Protocol.Objects.Selection
+            //    {
+            //        SelectionType = "registered"
+            //    },
+            //    Functions = new List<Protocol.Objects.Function>
+            //    {
+            //        new Protocol.Functions.SetHoldFunction
+            //        {
+            //            Params = new Protocol.Functions.SetHoldParams
+            //            {
+            //                HoldType = "nextTransition",
+            //                Fan = "on"
+            //            }
+            //        }
+            //    }
+            //};
+
+            //var themroFanResponse = client.PostAsync<ThermostatUpdateRequest, Response>(themroFanRequest).Result;
+            //Console.WriteLine();
+            //Console.WriteLine(JsonSerializer<Response>.Serialize(themroFanResponse));
 
             Console.ReadLine();
         }
